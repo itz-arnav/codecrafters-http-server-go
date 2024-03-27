36fa5fce54c64bb9cf0cfdf50974263de282e258
itz-arnav <arnavjha07@gmail.com>
2024-03-27 21:22:25 +0530

    server listening

--- app/server.go
+++ app/server.go
@@ -10,15 +10,34 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	server, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer server.Close()
 
-	_, err = l.Accept()
+	connection, err := server.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	defer connection.Close()
+
+	buf := make([]byte, 1024)
+	_, err = connection.Read(buf)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error while making buffer: %s", err)
+		os.Exit(1)
+	}
+
+	responseHeaders := "HTTP/1.1 200 OK\r\n\r\n" +
+		"Content-Type: text/html; charset=UTF-8\r\n\r\n"
+	_, err = connection.Write([]byte(responseHeaders))
+	if err != nil {
+		fmt.Println("Error writing HTTP header: ", err.Error())
+		os.Exit(1)
+	}
+
 }
